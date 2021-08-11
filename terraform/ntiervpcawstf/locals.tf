5f77b7211abc5fea8aeb0c0838e3612d3e372024
sireesha <sirikodali51@gmai.com>
2021-08-12 00:51:46 +0530

    changes with sg

--- terraform/ntiervpcawstf/locals.tf
+++ terraform/ntiervpcawstf/locals.tf
@@ -2,4 +2,9 @@ locals {
     subnetnames = ["web1", "web2", "app1", "app2", "db1", "db2"]
     igw_name = "ntierigw"
     anywhere = "0.0.0.0/0"
+    ssh = 22
+    appport = 8080
+    http = 80
+    tcp = "TCP"
+    dbport = 3306
 }
--- terraform/ntiervpcawstf/resource.tf
+++ terraform/ntiervpcawstf/resource.tf
@@ -1,4 +1,3 @@
-
 resource "aws_vpc" "ntier"{
 
     cidr_block = var.vpccidr
@@ -58,31 +57,20 @@ resource "aws_route_table" "publicrt" {
     ]
 }
 
-# resource "aws_route_table_association" "webassociation" {
-#   count = 2
-#   subnet_id      = aws_subnet.subnet[count.index].id
+resource "aws_route_table_association" "webassociation" {
+  count = 2
+  subnet_id      = aws_subnet.subnet[count.index].id
 
-#   route_table_id = aws_route_table.publicrt.id
+  route_table_id = aws_route_table.publicrt.id
 
-#     depends_on = [
-#        aws_subnet.subnet[0],
-#         aws_subnet.subnet[1],
-#         aws_route_table.publicrt
-#     ]
-# }
-
-resource "aws_route_table_association" "publicassociations"{
-
-for_each = data.aws_subnet_ids.publicsubnets.ids
-route_table_id = aws_route_table.publicrt.id
-subnet_id = each.key
-depends_on = [
+    depends_on = [
        aws_subnet.subnet[0],
-       aws_subnet.subnet[1],
-       aws_route_table.publicrt
+        aws_subnet.subnet[1],
+        aws_route_table.publicrt
     ]
-
 }
+
+
 
 
 resource "aws_route_table" "privatert" {
@@ -107,32 +95,107 @@ resource "aws_route_table" "privatert" {
 
 
 
-# resource "aws_route_table_association" "appassociation" {
-#   count = 4
-#   subnet_id      = aws_subnet.subnet[count.index+2].id
+resource "aws_route_table_association" "appassociation" {
+  count = 4
+  subnet_id      = aws_subnet.subnet[count.index+2].id
 
-#   route_table_id = aws_route_table.privatert.id
+  route_table_id = aws_route_table.privatert.id
 
-#     depends_on = [
-#        aws_subnet.subnet[2],
-#         aws_subnet.subnet[3],
-#         aws_subnet.subnet[4],
-#         aws_subnet.subnet[5],
-#         aws_route_table.privatert
-#     ]
-# }
-
-resource "aws_route_table_association" "privateassociations"{
-
-for_each = data.aws_subnet_ids.privatesubnets.ids
-route_table_id = aws_route_table.privatert.id
-subnet_id = each.key
-depends_on = [
+    depends_on = [
        aws_subnet.subnet[2],
-       aws_subnet.subnet[3],
-       aws_subnet.subnet[4],
-       aws_subnet.subnet[5],
-       aws_route_table.privatert
+        aws_subnet.subnet[3],
+        aws_subnet.subnet[4],
+        aws_subnet.subnet[5],
+        aws_route_table.privatert
     ]
+}
 
+resource "aws_security_group" "websg" {
+  name        = "websg"
+  description = "allow ports 22 and 80 from all"
+  vpc_id      = aws_vpc.ntier.id
+
+  ingress     {
+      description      = "allow all"
+      from_port        = local.ssh
+      to_port          = local.ssh
+      protocol         = local.tcp
+      cidr_blocks      = [local.anywhere]
+      
+    }
+  
+  ingress     {
+      description      = "allow all"
+      from_port        = local.http
+      to_port          = local.http
+      protocol         = local.tcp
+      cidr_blocks      = [local.anywhere]
+      
+    }
+  
+  tags = {
+    "Name" = "websgtf"
+  }
+  depends_on = [
+    aws_route_table.publicrt,
+    aws_route_table.privatert
+  ]
+
+}
+
+
+resource "aws_security_group" "appsg" {
+  name        = "appsg"
+  description = "allow ports 22 and 8080 from vpc range"
+  vpc_id      = aws_vpc.ntier.id
+
+  ingress     {
+      description      = "allow ssh port"
+      from_port        = local.ssh
+      to_port          = local.ssh
+      protocol         = local.tcp
+      cidr_blocks      = [var.vpccidr]
+      
+    }
+  
+  ingress      {
+      description      = "allow app port"
+      from_port        = local.appport
+      to_port          = local.appport
+      protocol         = local.tcp
+      cidr_blocks      = [var.vpccidr]
+      
+    }
+  
+  tags = {
+    "Name" = "appsgtf"
+  }
+  depends_on = [
+    aws_route_table.publicrt,
+    aws_route_table.privatert
+  ]
+}
+
+resource "aws_security_group" "dbsg" {
+  name        = "dbsg"
+  description = "allow ports 3306 from vpc range"
+  vpc_id      = aws_vpc.ntier.id
+
+  ingress     {
+      description      = "allow db port"
+      from_port        = local.dbport
+      to_port          = local.dbport
+      protocol         = local.tcp
+      cidr_blocks      = [var.vpccidr]
+      
+    }
+  
+  
+  tags = {
+    "Name" = "dbsgtf"
+  }
+  depends_on = [
+    aws_route_table.publicrt,
+    aws_route_table.privatert
+  ]
 }
